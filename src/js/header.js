7467afe5a4373b0769807c1437dda9c8349c2239
Daniel <puricchdaniil@gmail.com>
2026-07-15 11:40:59 +0300

    Add Tic Tac Toe SVG graphic for footer

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,8 @@
+import sunIcon from "../img/header/sun-switch.svg";
+import moonIcon from "../img/header/moon-switch.svg";
+
 const toggle = document.getElementById("themeToggle");
 const icon = document.querySelector(".theme-toggle__icon");
-
-const sunIcon = "../img/header/sun-switch.svg";
-const moonIcon = "../img/header/moon-switch.svg";
 
 const savedTheme = localStorage.getItem("theme") || "light";
 
@@ -16,36 +16,23 @@ if (savedTheme === "dark") {
 }
 
 toggle.addEventListener("click", () => {
-
-    const isDark =
-        document.documentElement.getAttribute("data-theme") === "dark";
+    const isDark = document.documentElement.getAttribute("data-theme") === "dark";
 
     icon.style.opacity = "0";
 
     setTimeout(() => {
-
         if (isDark) {
-
             document.documentElement.setAttribute("data-theme", "light");
             localStorage.setItem("theme", "light");
-
             toggle.classList.remove("active");
-
             icon.src = sunIcon;
-
         } else {
-
             document.documentElement.setAttribute("data-theme", "dark");
             localStorage.setItem("theme", "dark");
-
             toggle.classList.add("active");
-
             icon.src = moonIcon;
-
         }
 
         icon.style.opacity = "1";
-
     }, 120);
-
 });
